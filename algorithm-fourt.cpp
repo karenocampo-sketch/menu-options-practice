a4d3f7ded04413ee92fd1f409a532660cd86d8e6
Karen Ocampo <okaren.ojimena@gmail.com>
2024-05-26 12:07:10 -0500

    [A] Added new file excercise  4  of 50 : building

---
+++ algorithm-fourt.cpp
@@ -0,0 +1,14 @@
+// Prints even numbers from 1 to 100 
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    for (int i = 2; i <= 100; i += 2) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
