e490778dbc5b1b4605bdd6666083af48b0f52eaf
Karen Ocampo <okaren.ojimena@gmail.com>
2024-05-26 12:01:42 -0500

    [U] Update on the third exercise: construction : building

---
+++ algotithm-three.cpp
@@ -0,0 +1,13 @@
+// c++ algorithm that reads a number  of 100 al -2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    for (int i = 100; i >= -2; i--) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
